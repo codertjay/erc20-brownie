ca66431942d3a8681eb7b5c72ff1d370507f022f
codertjay <codertjay@gmail.com>
2021-11-18 08:04:32 +0100

    Initial commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,23 @@
+from brownie import (
+    network, accounts,
+    config
+)
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'ganache-local']
+FORKED_LOCAL_ENVIRONMENT = ['mainnet-fork', 'mainnet-fork-dev']
+
+
+def get_account(index=None, id=None):
+    # ganache account accounts[0]
+    # accounts.add(".env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or \
+            network.show_active() in FORKED_LOCAL_ENVIRONMENT:
+        return accounts[0]
+    return accounts.add(config['wallets']['from_key'])
